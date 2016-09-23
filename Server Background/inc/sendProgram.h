d516dab2fde56755c5578a4dba30a31ea2764850
Nathaniel7687 <nathaniel7687@gmail.com>
2016-09-23 14:53:04 +0900

    Modify module name.

---
+++ Server Background/inc/sendProgram.h
@@ -0,0 +1,1 @@
+void *thread_sendProgramToClient(void *data);
---
+++ Server Background/src/sendProgram.c
@@ -0,0 +1,12 @@
+#include "sendProgram.h"
+#include "webServer.h"
+
+void *thread_sendProgramToClient(void *data)
+{
+    // printf("> Create manageProgramFromServer thread\n");
+
+    // while (true) {
+    // }
+
+    pthread_exit((void *)0);
+}
